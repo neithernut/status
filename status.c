32e90ff11871ced845a7b27db202cc721e128b36
Julian Ganz <neither@nut.email>
2022-10-07 23:51:27 +0200

    Add a buffer data type for assembling a line of output
    We'll write individual lines of output assembled from a few pieces. We
    know that the line will be reasonably short and thus decided to use a
    statically allocated buffer of a fixed size.

--- status.c
+++ status.c
@@ -1,6 +1,54 @@
 // SPDX-License-Identifier: MIT
 // Copyright Julian Ganz 2022
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+
+struct buf {
+    size_t len;
+    char data[120];
+};
+
+void buf_reset(struct buf* b) {
+    b->len = 0;
+}
+
+size_t buf_remaining(const struct buf* b) {
+    if (b->len >= sizeof(b->data))
+        return 0;
+    return sizeof(b->data) - b->len;
+}
+
+int buf_bump(struct buf* b, int chars) {
+    if (chars > 0)
+        b->len += chars;
+    return chars;
+}
+
+int buf_append(struct buf* b, const char *s) {
+    size_t len = strlen(s);
+    if (len > buf_remaining(b))
+        return 0;
+    memcpy(b->data + b->len, s, len);
+    return buf_bump(b, len);
+}
+
+int buf_printf(struct buf* b, const char *format, ...) {
+    size_t rem = buf_remaining(b);
+    if (!rem)
+        return 0;
+
+    va_list ap;
+
+    va_start(ap, format);
+    int retval = vsnprintf(b->data + b->len, rem, format, ap);
+    va_end(ap);
+
+    return buf_bump(b, retval);
+}
+
 
 int main() {
 }
