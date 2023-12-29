a0c69447994a1076816c100e5162bceefd9d908e
Julian Ganz <neither@nut.email>
2023-12-29 01:01:36 +0100

    Print current local time in an infinite loop

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 // SPDX-License-Identifier: MIT
 // Copyright Julian Ganz 2023
 
-use anyhow::Result;
+use anyhow::{Context, Result};
+
+/// Format for datetimes
+const DATETIME_FORMAT: &[time::format_description::FormatItem] =
+    time::macros::format_description!("[year]-[month]-[day] [hour]:[minute]:[second]");
 
 fn main() -> Result<()> {
-    Ok(())
+    use std::io::Write;
+
+    let mut output_buffer: Vec<u8> = Default::default();
+    loop {
+        // We re-use the buffer in order to avoid repeated allocations. Which
+        // means we need to clear it manually.
+        output_buffer.clear();
+
+        time::OffsetDateTime::now_local()
+            .context("Could not get current time")?
+            .format_into(&mut output_buffer, DATETIME_FORMAT)
+            .context("Could not format current time")?;
+
+        output_buffer
+            .write_all(b"\n")
+            .context("Could not finalize line")?;
+        std::io::stdout()
+            .write_all(output_buffer.as_ref())
+            .context("Could not print status line")?;
+    }
 }
