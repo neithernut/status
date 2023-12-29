6902f3bcf165c7fd1ca6914e106ea2e09b7f0aef
Julian Ganz <neither@nut.email>
2023-12-29 01:05:36 +0100

    main: basic setup for prodicing a line every second
    ... early in that second
    
    One core functionality of the original `status` is printing one line
    every second, early in that wallclock second. This change introduces the
    basic setup to do just that. Currently, that line remains empty, but
    future changes will allow for configurable contents.
    
    We would have liked to do writing to stdout and waiting for the next
    tick via the IO uring as well. Given the `IOSQE_IO_DRAIN` drain flag
    it should be possible to only issue one set of submission queue events
    per tick that "do it all". However, when we actually tried this we
    observed some bad behaviour and absurdly huge IO loads. So for now, we
    do these things the classic way. We may still go for "full IO uring" in
    the future.
    
    Another decision we made delibarately was to not write to stdout
    directly but to fill a buffer first. This should safe us some repeated
    allocations. And if we want to write those lines via the IO uring in the
    future, we'll need to handle the output buffer ourselves anyway.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 // Copyright Julian Ganz 2024
 
-use anyhow::Result;
+use anyhow::{Context, Error, Result};
+use rustix::{io::Errno, time};
 
 mod entry;
 mod read;
@@ -9,5 +10,55 @@ mod source;
 mod spec;
 
 fn main() -> Result<()> {
-    Ok(())
+    use std::io::Write;
+
+    // Timer ticking on wallclock seconds
+    let timer = time::timerfd_create(time::TimerfdClockId::Realtime, time::TimerfdFlags::CLOEXEC)
+        .context("Could not create timer")?;
+    arm_timer(&timer)?;
+
+    let mut output_buffer: Vec<u8> = Default::default();
+    loop {
+        // We re-use the buffer in order to avoid repeated allocations. Which
+        // means we need to clear it manually.
+        output_buffer.clear();
+
+        match rustix::io::read_uninit(&timer, &mut [core::mem::MaybeUninit::uninit(); 8]) {
+            Ok(_) => (),
+            Err(Errno::CANCELED) => arm_timer(&timer)?,
+            Err(e) => return Err(Error::new(e).context("Broken timer")),
+        };
+
+        writeln!(output_buffer, "").context("Could not format line")?;
+        std::io::stdout()
+            .write_all(output_buffer.as_ref())
+            .context("Could not print status line")?;
+    }
+}
+
+/// One second as a [time::Timespec]
+const TIMESPEC_SECOND: time::Timespec = time::Timespec {
+    tv_sec: 1,
+    tv_nsec: 0,
+};
+
+/// Arm a timer to tick on exact wallclock seconds
+fn arm_timer(timer: impl rustix::fd::AsFd + Copy) -> Result<()> {
+    use time::TimerfdTimerFlags as TimerFlags;
+
+    loop {
+        let mut current = time::clock_gettime(time::ClockId::Realtime);
+        current.tv_nsec = 0;
+
+        const FLAGS: TimerFlags = TimerFlags::union(TimerFlags::ABSTIME, TimerFlags::CANCEL_ON_SET);
+        let spec = time::Itimerspec {
+            it_interval: TIMESPEC_SECOND,
+            it_value: current,
+        };
+        match time::timerfd_settime(timer, FLAGS, &spec) {
+            Ok(_) => return Ok(()),
+            Err(Errno::CANCELED) => (),
+            Err(e) => return Err(Error::new(e).context("Could not arm timer")),
+        }
+    }
 }
