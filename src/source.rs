5666049ffec1fba663369f1e1b4fd8bd1c45cdeb
Julian Ganz <neither@nut.email>
2024-05-01 21:39:43 +0200

    source: add a source for a single word
    The original C version would allow extracting the first value (or word)
    from `/proc/loadavg`. Since we want to provide the same functionality in
    the Rust version, we need a `Source` providing that functionality. And
    because it takes virtually no additional effort and may come in handy
    with other files, we make it more generic than the old version that
    could only retrieve the value verbatim.

--- src/source.rs
+++ src/source.rs
@@ -3,6 +3,9 @@
 //! Sources for typed values
 
 use std::borrow::Borrow;
+use std::str::FromStr;
+
+use crate::read::BufProcessor;
 
 /// A source for values
 pub trait Source {
@@ -16,4 +19,30 @@ pub trait Source {
 
     /// Retrieve the (current) value from this source
     fn value(&self) -> Option<Self::Borrow<'_>>;
+}
+
+/// Source for a single (parsed) word extracted from a buffer
+#[derive(Default)]
+pub struct Word<T: FromStr + Clone> {
+    data: Option<T>,
+}
+
+impl<T: FromStr + Clone> Source for Word<T> {
+    type Value = T;
+
+    type Borrow<'a> = Self::Value where Self::Value: 'a;
+
+    fn value(&self) -> Option<Self::Borrow<'_>> {
+        self.data.clone()
+    }
+}
+
+impl<T: FromStr + Clone> BufProcessor for Word<T> {
+    fn process(&mut self, buf: &[u8]) {
+        self.data = buf
+            .split(u8::is_ascii_whitespace)
+            .find(|w| !w.is_empty())
+            .and_then(|w| std::str::from_utf8(w).ok())
+            .and_then(|s| s.parse().ok());
+    }
 }
