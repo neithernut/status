fe3a35ef251f3a033e1037ca3bc2974dd813ee70
Julian Ganz <neither@nut.email>
2024-05-01 23:03:47 +0200

    source: add extractor specifically for PSI info
    The original C version would allow extracting 10min average pressure
    info. Since we want to provide the same functionality in the Rust
    version, we need a `Source` providing that functionality. Since the
    average is provided as a rational number, we decided to parse it as a
    floating point. An `f32` is sufficient since the numbers are not that
    precise to begin with, and we don't do any calculations with them.

--- src/source.rs
+++ src/source.rs
@@ -46,3 +46,31 @@ impl<T: FromStr + Clone> BufProcessor for Word<T> {
             .and_then(|s| s.parse().ok());
     }
 }
+
+/// Source for a 10min average PSI info
+#[derive(Default)]
+pub struct PSI {
+    data: Option<f32>,
+}
+
+impl Source for PSI {
+    type Value = f32;
+
+    type Borrow<'a> = Self::Value;
+
+    fn value(&self) -> Option<Self::Borrow<'_>> {
+        self.data
+    }
+}
+
+impl BufProcessor for PSI {
+    fn process(&mut self, buf: &[u8]) {
+        self.data = buf
+            .split(|c| *c == b'\n')
+            .filter_map(|l| l.strip_prefix(b"some"))
+            .flat_map(|l| l.split(u8::is_ascii_whitespace))
+            .filter_map(|w| w.strip_prefix(b"avg10="))
+            .find_map(|w| std::str::from_utf8(w).ok())
+            .and_then(|s| s.parse().ok());
+    }
+}
