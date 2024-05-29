1ebd6c09c815dbd7629d03c1ef4dd3f58d4b4389
Julian Ganz <neither@nut.email>
2024-05-29 18:59:43 +0200

    deps: switch from iou to io-uring
    `iou` isn't well maintained. The last time it received any updates was
    more than three and a half years ago. We still chose it over alternative
    uring crates because it was the only crate which featured a fn for
    awaiting a single completion queue event, specifically.
    
    However, when looking at what actually happens w.r.t. syscalls being
    made, there is no additional cost involved in submitting _and_ awaiting
    some number of completion queue events. It's just that our own code
    becomes a bit uglier since, with most abstractions, we always will get
    some form of `Iterator`.
    
    In other words, the reason why we used iou rather than io_uring didn't
    really exist. And as it turned out, current `iou` isn't usable on musl
    based systems because of the `uring-sys` dependency which was maintained
    and seeminlgy abandoned by the same group of people.
    
    Luckily, everything uring-specific is currently contained in the `read`
    module. Thus, even though the API is largely completely different, the
    changes we need to make in order to switch to `io-uring` are far more
    localized as the importance of this dependency would indicate.
    
    We did it all in one go, since there isn't really any sensible way to
    partition these changes. We _do_ want each revision to build, after all.

--- src/read.rs
+++ src/read.rs
@@ -41,16 +41,18 @@ impl Item {
         }
     }
 
-    /// Prepare an [iou::SQE]
+    /// Prepare an [io_uring::squeue::Entry]
     ///
-    /// The [iou::SQE] will repared such that it will read the (internally held)
-    /// file into the (internally held) buffer.
-    pub unsafe fn prepare(&mut self, sqe: &mut iou::SQE) {
-        sqe.prep_read(self.file.as_raw_fd(), Pin::into_inner(self.buf.as_mut()), 0)
+    /// The [io_uring::squeue::Entry] returned will read the (internally held)
+    /// file into the (internally held) buffer when submitted.
+    pub fn prepare(&mut self) -> io_uring::squeue::Entry {
+        let fd = io_uring::types::Fd(self.file.as_raw_fd());
+        let buf = Pin::into_inner(self.buf.as_mut());
+        io_uring::opcode::Read::new(fd, buf.as_mut_ptr(), buf.len().try_into().unwrap()).build()
     }
 
-    /// Process the result of a previously prepared [iou::SQE]
-    pub fn process(&self, result: std::io::Result<u32>) -> Result<()> {
+    /// Process the result of a previously prepared [io_uring::squeue::Entry]
+    pub fn process(&self, result: rustix::io::Result<u32>) -> Result<()> {
         let length = result
             .context("Operation failed")?
             .try_into()
@@ -68,54 +70,64 @@ impl Item {
 
 /// Abstraction of an IO uring processing a set of [Item]s multiple times
 pub struct Ring {
-    ring: iou::IoUring,
+    ring: io_uring::IoUring,
     items: Vec<Item>,
 }
 
 impl Ring {
     /// Prepare submission queue events for all [Item]s
     pub fn prepare(&mut self) -> Result<()> {
-        let sqes = self
-            .len()
-            .try_into()
-            .context("Could not prepare enough SQEs")?;
-        let mut sq = self.ring.sq();
-        let sqes = sq
-            .prepare_sqes(sqes)
-            .context("Could not get submission queue events for preparation")?;
+        let mut sq = self.ring.submission();
         self.items
             .iter_mut()
-            .zip(sqes)
             .zip(0..)
-            .for_each(|((t, mut e), n)| unsafe {
-                t.prepare(&mut e);
-                e.set_user_data(n);
-            });
+            .map(|(t, n)| t.prepare().user_data(n))
+            .try_for_each(|e| unsafe { sq.push(&e) })
+            .context("Could not prepare SQEs")?;
+        sq.sync();
         Ok(())
     }
 
     /// Submit events and dispatch completion events
     pub fn submit_and_dispatch(&mut self) -> Result<()> {
-        let submitted = self
-            .ring
-            .submit_sqes()
-            .context("Could not submit read items")?;
-        for _ in 0..submitted {
-            let cqe = self
+        let mut submitted = 0;
+        loop {
+            // We could submit and wait for all the items to complete with a
+            // single call to `io_uring::IoUring::submit_and_wait`, but we want
+            // to process any result as soon as possible. We therefore await a
+            // single completion event. Still, we need to ensure that we process
+            // all items we submitted, eventually.
+            submitted = self
                 .ring
-                .wait_for_cqe()
-                .context("Could not get read item result")?;
-            let id: usize = cqe
-                .user_data()
-                .try_into()
-                .with_context(|| format!("Encountered invalid item id {}", cqe.user_data()))?;
-            self.items
-                .get(id)
-                .context("Could not find associated read item")?
-                .process(cqe.result())
-                .context("Item with id {id} failed")?;
+                .submit_and_wait(1)
+                .context("Could not submit/wait for completions")?
+                .checked_add(submitted)
+                .context("Accumulated too many submitted items")?;
+
+            let mut completion = self.ring.completion();
+            completion.sync();
+            submitted = submitted.saturating_sub(completion.len());
+
+            completion.try_for_each(|e| {
+                let id: usize = e
+                    .user_data()
+                    .try_into()
+                    .with_context(|| format!("Encountered invalid item id {}", e.user_data()))?;
+                let result = e
+                    .result()
+                    .try_into()
+                    .map_err(|_| rustix::io::Errno::from_raw_os_error(e.result().wrapping_neg()));
+                self.items
+                    .get(id)
+                    .context("Could not find associated read item")?
+                    .process(result)
+                    .with_context(|| format!("Item with id {id} failed"))
+            })?;
+
+            if submitted == 0 {
+                break Ok(());
+            }
         }
-        Ok(())
     }
 
     /// Retrieve the number of [Item]s
@@ -134,7 +146,7 @@ impl TryFrom<Vec<Item>> for Ring {
             .max(1)
             .try_into()
             .context("Too many items for one ring")?;
-        let ring = iou::IoUring::new(num).context("Could not create IO uring")?;
+        let ring = io_uring::IoUring::new(num).context("Could not create IO uring")?;
         Ok(Self { ring, items })
     }
 }
