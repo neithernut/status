b1500cf082d79677b8f23300ad83449c36b5b7a3
Julian Ganz <neither@nut.email>
2024-05-31 15:49:22 +0200

    meminfo: impl read::BufProcessor for MemInfo
    We intend to support displaying some values from `/proc/meminfo` as part
    of the status line. For that purpose, we already introduced a container
    for those values in a dedicated module which is also meant to hold any
    impls associated with that type.
    
    And because this is kind-of special, with a very narrow use-case, we
    already implemented `Source` for `MemInfo` directly. And the same
    rationale applies to `BufProcessor`.

--- src/meminfo.rs
+++ src/meminfo.rs
@@ -2,6 +2,7 @@
 // Copyright Julian Ganz 2024
 //! Utilities for handling `/proc/meminfo`
 
+use crate::read::BufProcessor;
 use crate::source::Source;
 
 /// Values found in `/proc/meminfo`
@@ -17,6 +18,32 @@ impl Source for MemInfo {
     }
 }
 
+impl BufProcessor for MemInfo {
+    fn process(&mut self, buf: &[u8]) {
+        *self = buf
+            .split(|c| *c == b'\n')
+            .map(std::str::from_utf8)
+            .filter_map(Result::ok)
+            .filter_map(|l| l.split_once(':'))
+            .filter_map(|(k, v)| {
+                let key = match k {
+                    "MemTotal" => Item::Total,
+                    "MemFree" => Item::Free,
+                    "MemAvailable" => Item::Avail,
+                    "SwapTotal" => Item::SwapTotal,
+                    "SwapFree" => Item::SwapFree,
+                    _ => return None,
+                };
+                let value = v
+                    .strip_suffix("kB")
+                    .map(str::trim)
+                    .and_then(|v| v.parse().ok());
+                Some((key, value))
+            })
+            .collect();
+    }
+}
+
 /// Identification of a [MemInfo] item
 #[derive(Copy, Clone, enum_map::Enum)]
 pub enum Item {
@@ -25,4 +52,56 @@ pub enum Item {
     Avail,
     SwapTotal,
     SwapFree,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn smoke() {
+        let buf = concat!(
+            "MemTotal:       32166268 kB\n",
+            "MemFree:        27646592 kB\n",
+            "MemAvailable:   30632568 kB\n",
+            "Buffers:          432404 kB\n",
+            "Cached:          2595616 kB\n",
+            "Mlocked:           11808 kB\n",
+            "SwapTotal:          1024 kB\n",
+            "SwapFree:              0 kB\n",
+            "Dirty:                36 kB\n",
+            "Writeback:             0 kB\n",
+            "AnonPages:        459056 kB\n",
+            "Mapped:           376292 kB\n",
+            "Shmem:             96444 kB\n",
+        )
+        .as_bytes();
+        let mut meminfo = MemInfo::default();
+        meminfo.process(buf);
+        assert_eq!(meminfo[Item::Total], Some(32166268));
+        assert_eq!(meminfo[Item::Free], Some(27646592));
+        assert_eq!(meminfo[Item::Avail], Some(30632568));
+        assert_eq!(meminfo[Item::SwapTotal], Some(1024));
+        assert_eq!(meminfo[Item::SwapFree], Some(0));
+    }
+
+    #[test]
+    fn incomplete() {
+        let buf = concat!(
+            "MemTotal:       32166268 kB\n",
+            "MemFree:        27646592 kB\n",
+            "MemAvailable:   30632568 kB\n",
+            "Buffers:          432404 kB\n",
+        )
+        .as_bytes();
+        let mut meminfo = MemInfo::default();
+        meminfo[Item::SwapTotal] = Some(0);
+        meminfo[Item::SwapFree] = Some(0);
+        meminfo.process(buf);
+        assert_eq!(meminfo[Item::Total], Some(32166268));
+        assert_eq!(meminfo[Item::Free], Some(27646592));
+        assert_eq!(meminfo[Item::Avail], Some(30632568));
+        assert_eq!(meminfo[Item::SwapTotal], None);
+        assert_eq!(meminfo[Item::SwapFree], None);
+    }
 }
