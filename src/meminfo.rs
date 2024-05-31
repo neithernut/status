61fc6331393321eb73f50939a7aed82906533271
Julian Ganz <neither@nut.email>
2024-05-31 15:49:44 +0200

    meminfo: impl Source for MemInfo
    We intend to support displaying some values from `/proc/meminfo` as part
    of the status line. For that purpose, we already introduced a container
    for those values in a dedicated module which is also meant to hold any
    impls associated with that type.
    
    And because this is kind-of special, with a very narrow use-case, there
    is not really any point in introducing a new type serving as a `Source`
    when it only holds an already very specific `MemInfo`.
    
    We therefore decided to impl `Source` for that type directly.

--- src/meminfo.rs
+++ src/meminfo.rs
@@ -2,8 +2,20 @@
 // Copyright Julian Ganz 2024
 //! Utilities for handling `/proc/meminfo`
 
+use crate::source::Source;
+
 /// Values found in `/proc/meminfo`
 pub type MemInfo = enum_map::EnumMap<Item, Option<u64>>;
+
+impl Source for MemInfo {
+    type Value = Self;
+
+    type Borrow<'a> = &'a Self::Value;
+
+    fn value(&self) -> Option<Self::Borrow<'_>> {
+        Some(self)
+    }
+}
 
 /// Identification of a [MemInfo] item
 #[derive(Copy, Clone, enum_map::Enum)]
