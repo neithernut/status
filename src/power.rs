212f218be79c94bb80cf93ed805031423e574411
Julian Ganz <neither@nut.email>
2024-06-11 22:30:58 +0200

    power: add fn for retrieving the kind of power supply
    We're primarily interested in batteries. At least for now. And even if
    we're interested in anything else in the future, knowing what kind of
    power supply we have is information we're definitely interrested in.

--- src/power.rs
+++ src/power.rs
@@ -2,6 +2,8 @@
 // Copyright Julian Ganz 2024
 //! Uitilities related to entities in `/sys/class/power_supply`
 
+use std::fs::File;
+use std::io::Read;
 use std::path;
 
 use anyhow::{Context, Result};
@@ -16,6 +18,22 @@ impl Supply {
     /// Get the name of this power supply
     pub fn name(&self) -> &str {
         self.name.as_ref()
+    }
+
+    /// Get the kind of power supply
+    pub fn kind(&self) -> Result<Kind> {
+        let mut buf = String::new();
+        self.open_file("type")?
+            .read_to_string(&mut buf)
+            .context("Could not determining type")?;
+        buf.trim().parse().context("Could not parse type")
+    }
+
+    /// Open a specific file
+    fn open_file(&self, name: &str) -> Result<File> {
+        self.dir
+            .open_file(name)
+            .with_context(|| format!("Could not open '{name}'"))
     }
 
     fn new(name: String, path: impl AsRef<path::Path>) -> Result<Self> {
@@ -33,5 +51,30 @@ impl std::str::FromStr for Supply {
         let mut path = path::PathBuf::from("/sys/class/power_supply/");
         path.push(s);
         Self::new(s.into(), path)
+    }
+}
+
+/// Kind of power supply
+#[derive(Copy, Clone, PartialEq)]
+pub enum Kind {
+    Battery,
+    UPS,
+    Mains,
+    USB,
+    Wireless,
+}
+
+impl std::str::FromStr for Kind {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Battery" => Ok(Self::Battery),
+            "UPS" => Ok(Self::UPS),
+            "Mains" => Ok(Self::Mains),
+            "USB" => Ok(Self::USB),
+            "Wireless" => Ok(Self::Wireless),
+            s => Err(anyhow::anyhow!("Invalid kind '{s}'")),
+        }
     }
 }
